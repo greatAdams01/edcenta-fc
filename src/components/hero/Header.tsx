6333fea6992b2d29da2a0cd7ba5e70a813cf074e
unknown <borisadedayopaul@gmail.com>
2024-02-03 20:35:07 +0100

    32% done witht the frontend

--- src/components/hero/Header.tsx
+++ src/components/hero/Header.tsx
@@ -24,7 +24,7 @@ export function Header() {
       <Container className="flex flex-wrap items-center justify-center sm:justify-between lg:flex-nowrap">
         <div className="flex items-center mt-10 lg:mt-0 lg:grow lg:basis-0">
           <Image src="/logo.png" alt="Logo" width={100} height={100} />
-          <h1 className='text-purple-500 text-xl font-bold m-2'>EdCenta</h1>
+          <h1 className='text-yellow-500 text-xl font-bold m-2'>EdCenta</h1>
         </div>
         <div className='flex w-[60%]'>
           <Nav />
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,21 +1,24 @@
-// import { Button } from '@/components/Button'
-import { Container } from '@/components/Container'
-import Button from '@/utils/button'
+import { Container } from '@/components/Container';
+import Button from '@/utils/button';
 
 export function Hero() {
   return (
-    <div className="relative py-20 w-full h-[85vh] sm:pb-24 sm:pt-36 border-2 border-red border-solid" style={{ backgroundImage: "url('/istockphoto-1096013738-612x612.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
-        <div className="mx-48 max-w-2xl lg:max-w-4xl lg:px-12 w-full">
-          <div className='w-[50%] mt-2'>
-            <h1 className='text-[4rem] font-bold'>EdCenta</h1>
-            <p className='mt-2 text-[2rem]'>Lorem ipsum dolor sit amet consectetur.</p>
-            <p className='mt-2 text-[1rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint odio laboriosam, itaque perferendis fuga reprehenderit ipsum voluptate consequatur? Amet tenetur quo ratione quae numquam molestiae mollitia beatae vel fuga. Qui totam dolore explicabo pariatur nobis, odio assumenda quos dolorem consequatur, voluptatem ducimus architecto labore quia incidunt quasi ipsa officia ratione.</p>
-            <div className='mt-6'>
-              <Button />
-            </div>
+    <div className="relative py-20 w-full h-[85vh] sm:pb-24 sm:pt-36 border-2 border-red border-solid overflow-hidden" style={{ backgroundImage: "url('/istockphoto-1096013738-612x612.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className="w-3/5 h-[50rem] mt-[-9rem] ml-[-13rem] bg-yellow-500 opacity-20 absolute z-0 origin-center rotate-[30deg] rounded-lg" />
+      <div className="w-3/5 h-[50rem] mt-[-9rem] ml-[-13rem] bg-blue-500 opacity-40 absolute z-0 origin-center rotate-[53deg] rounded-lg" />
+      <div className="w-3/5 h-[50rem] mt-[-9rem] ml-[-13rem] bg-purple-500 opacity-50 backdrop-blur-sm absolute z-0 origin-center rotate-[50deg] rounded-lg" />
+      <div className="mx-48 max-w-2xl lg:max-w-4xl lg:px-12 w-full relative z-auto">
+        <div className='w-[50%] mt-2'>
+          <h1 className='text-[4rem] font-bold relative' style={{ backgroundImage: "url('/background.jpg')", WebkitBackgroundClip: 'text', color: 'transparent' }}>
+            EdCenta
+          </h1>
+          <p className='mt-2 text-[2rem]'>Lorem ipsum dolor sit amet consectetur.</p>
+          <p className='mt-2 text-[1rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint odio laboriosam, itaque perferendis fuga reprehenderit ipsum voluptate consequatur? Amet tenetur quo ratione quae numquam molestiae mollitia beatae vel fuga. Qui totam dolore explicabo pariatur nobis, odio assumenda quos dolorem consequatur, voluptatem ducimus architecto labore quia incidunt quasi ipsa officia ratione.</p>
+          <div className='mt-6'>
+            <Button />
           </div>
-          
         </div>
+      </div>
     </div>
-  )
+  );
 }
