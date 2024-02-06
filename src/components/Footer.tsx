8ff8ab6e1bca4dbdc5e43e91c5b5ef745eee8830
unknown <borisadedayopaul@gmail.com>
2024-02-06 20:46:07 +0100

    95% done with the footer, yet to link them

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,12 @@ import { Facebook, Linkedin, Instagram, Twitter, Youtube } from 'lucide-react'
 export function Footer() {
   return (
     <footer className="w-full text-sm ">
-      <Container className="flex justify-between my-8">
-        <div>
-          <Image src={'/Footerlogo.png'} alt='EdCenta' width={100} height={100} className='w-36'/>
+      
+      <Container className="grid grid-cols-[repeat(auto-fit,_minmax(10rem,_1fr))]  justify-between my-8  items-start md:items-end" >
+        <div className='mb-2'>
+        <div >
+          <Image src={'/Footerlogo.png'} alt='EdCenta' width={100} height={100} className='w-36 my-4'/>
         </div>
-        <div>
           <p className='font-bold'>Who we help</p>
           <ul className='leading-loose'>
             <li>School</li>
@@ -18,7 +19,7 @@ export function Footer() {
             <li>Students</li>
           </ul>
         </div>
-        <div>
+        <div className='mb-2'>
           <p className='font-bold'>Company</p>
           <ul className='leading-loose'>
             <li>About EdCenta</li>
@@ -29,7 +30,7 @@ export function Footer() {
             <li>Contact</li>
           </ul>
         </div>
-        <div>
+        <div className='mb-2'>
         <p className='font-bold'>Socials</p>
           <ul className='leading-loose'>
             <li>Linkedin</li>
@@ -39,7 +40,7 @@ export function Footer() {
             <li>X (Twitter)</li>
           </ul>
         </div>
-        <div>
+        <div className='mb-2'>
         <p className='font-bold'>Support</p>
           <ul className='leading-loose'>
             <li>Webinars</li>
@@ -52,32 +53,33 @@ export function Footer() {
           </ul>
         </div>
       </Container>
-      <Container className="flex justify-center">
-        <div className='w-4/5 '>
+      <Container className="lg:flex justify-center">
+        <div className='w-4/5 leading-loose'>
           <hr />
-          <div className=' flex justify-between items-center my-2'>
-            <div className='flex justify-between w-2/6'>
+          <div className='block sm:flex justify-between items-center my-2'>
+            <div className='order-last w-full flex items-center justify-between mt-4 sm:mt-0 sm:w-2/6'>
+              <p>Follow us</p>
+              <div className='bg-[#0045BC] text-white p-1 lg:p-2 rounded-md cursor-pointer'>
+                <Facebook />
+              </div>
+              <div className='bg-[#0045BC] text-white p-1 lg:p-2 rounded-md cursor-pointer'>
+                <Linkedin />
+              </div>
+              <div className='bg-[#0045BC] text-white p-1 lg:p-2 rounded-md cursor-pointer'>
+                <Instagram />
+              </div>
+              <div className='bg-[#0045BC] text-white p-1 lg:-2 rounded-md cursor-pointer'>
+                <Twitter />
+              </div>
+              <div className='bg-[#0045BC] text-white p-1 lg:p-2 rounded-md cursor-pointer'>
+                <Youtube />
+              </div>
+            </div>
+            
+            <div className='order-first w-full flex justify-between sm:w-2/6'>
               <p>Terms & Conditions</p>
               <p>Privacy</p>
               <p>Help</p>
-            </div>
-            <div className='flex items-center justify-between w-2/6'>
-              <p>Follow us</p>
-              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
-              <Facebook />
-              </div>
-              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
-              <Linkedin />
-              </div>
-              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
-              <Instagram />
-              </div>
-              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
-              <Twitter />
-              </div>
-              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
-              <Youtube />
-              </div>
             </div>
           </div>
         </div>
