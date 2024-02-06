9acbdba14fe8baafe522f9480222ee6301c91482
unknown <borisadedayopaul@gmail.com>
2024-02-06 18:57:27 +0100

    Completed the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,86 @@
 import { Container } from '@/components/Container'
 import Image from 'next/image'
+import { Facebook, Linkedin, Instagram, Twitter, Youtube } from 'lucide-react'
 
 export function Footer() {
   return (
-    <footer className="flex-none mt-4 py-4 bg-[#0045BC]">
-      <Container className="flex flex-col items-center justify-between md:flex-row">
-      <div className="flex items-center mt-10 lg:mt-0 lg:grow lg:basis-0">
-          <Image src="/logo.png" alt="Logo" width={100} height={100} className='w-20' />
-          <h1 className='text-yellow-500 text-xl font-bold m-2'>EdCenta</h1>
+    <footer className="w-full text-sm ">
+      <Container className="flex justify-between my-8">
+        <div>
+          <Image src={'/Footerlogo.png'} alt='EdCenta' width={100} height={100} className='w-36'/>
         </div>
-        <p className="mt-6 text-base text-white md:mt-0">
-          Copyright &copy; {new Date().getFullYear()} EdCenta, LLC. All
-          rights reserved.
-        </p>
+        <div>
+          <p className='font-bold'>Who we help</p>
+          <ul className='leading-loose'>
+            <li>School</li>
+            <li>Teachers</li>
+            <li>Parents</li>
+            <li>Students</li>
+          </ul>
+        </div>
+        <div>
+          <p className='font-bold'>Company</p>
+          <ul className='leading-loose'>
+            <li>About EdCenta</li>
+            <li>Blog</li>
+            <li>Careers</li>
+            <li>Awards</li>
+            <li>Investors</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+        <p className='font-bold'>Socials</p>
+          <ul className='leading-loose'>
+            <li>Linkedin</li>
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>Youtube</li>
+            <li>X (Twitter)</li>
+          </ul>
+        </div>
+        <div>
+        <p className='font-bold'>Support</p>
+          <ul className='leading-loose'>
+            <li>Webinars</li>
+            <li>Classroom resources</li>
+            <li>Curicullum alignment</li>
+            <li>Partner support</li>
+            <li>Technical support</li>
+            <li>Blog</li>
+            <li>FAQs</li>
+          </ul>
+        </div>
+      </Container>
+      <Container className="flex justify-center">
+        <div className='w-4/5 '>
+          <hr />
+          <div className=' flex justify-between items-center my-2'>
+            <div className='flex justify-between w-2/6'>
+              <p>Terms & Conditions</p>
+              <p>Privacy</p>
+              <p>Help</p>
+            </div>
+            <div className='flex items-center justify-between w-2/6'>
+              <p>Follow us</p>
+              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
+              <Facebook />
+              </div>
+              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
+              <Linkedin />
+              </div>
+              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
+              <Instagram />
+              </div>
+              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
+              <Twitter />
+              </div>
+              <div className='bg-[#0045BC] text-white  p-2 rounded-md cursor-pointer'>
+              <Youtube />
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </footer>
   )
